2fae026c054ded662319b9c6ee4e0254103980e8
Kry1X <bogdankrylosov08@gmail.com>
2025-03-06 23:16:33 +0300

    Created bot

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const TelegramApi = require('node-telegram-bot-api')
 const token = process.env.TOKEN
 const bot = new TelegramApi(token, {polling: true})
 
-const chat = {}
+const chatsId = new Set()
 
 const getYearProgress = async () =>  {
     const now = new Date();
@@ -18,34 +18,42 @@ const getYearProgress = async () =>  {
 
 const sendNotification = async () => {
     const progress = await getYearProgress()
-    await bot.sendMessage(chatId, `<b>The year 2025 is completed by <u>${progress}</u></b>🕔`, {parse_mode: 'HTML'})
+    for (const chatId of chatsId) {
+        try {
+            console.log(`Message to chat: ${chatId} send`)
+            await bot.sendMessage(chatId, `<b>The year 2025 is completed by <u>${progress}</u></b>🕔`, {parse_mode: 'HTML'})
+        } catch (e) {
+            console.error(`ChatId: ${chatsId}; Error: ${e}`)
+        }
+    }
+    
 }
 
-schedule.scheduleJob("40 22 * * *", () => {
+schedule.scheduleJob("00 10 * * *", () => {
     console.log('Send message to users...')
     sendNotification();
 })
 
 const start = async () => {
-    // bot.setMyCommands([
-    //     {command: '/progress', description: 'Time'}
-    // ])
-
     bot.on('message', async msg => {
         const text = msg.text
         const chatId = msg.chat.id
 
+        chatsId.add(chatId)
         try {
             if (text === '/start') {
                 return bot.sendMessage(chatId, `Hello, <b>${msg.chat.username}</b>👋`, {parse_mode: 'HTML'})
             }
             else if (text === '/progress') {
-                sendNotification()
+                const progress = await getYearProgress();
+                await bot.sendMessage(chatId, `<b>2025 is completed by ${progress}</b>`, {parse_mode: 'HTML'})
             }
         } catch(e) {
-            return bot.sendMessage(chatId, 'Error!');
+            console.error((chatId, `Error: ${e}`))
+            return bot.sendMessage(chatId, `Error!`);
         }
     })
 }
 
 start()
+console.log('=======> Server started <=======')
